5d9bc4c96a15e7cc95c3250b4a6940827f8d3162
prafsoni <prafsoni@gmail.com>
2020-03-21 22:49:55 -0500

    [chore] update dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,12 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ElasticLog",
-            targets: ["ElasticLog"]),
+            targets: ["ElasticLog"]
+        ),
+        .library(
+            name: "ElasticNIOClient",
+            targets: ["ElasticNIOClient"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,9 +28,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ElasticLog",
-            dependencies: ["Logging", "NIO", "NIOConcurrencyHelpers", "NIOSSL"]),
+            dependencies: ["Logging", "NIO", "NIOConcurrencyHelpers", "NIOSSL", "ElasticNIOClient"]
+        ),
+        .target(
+            name: "ElasticNIOClient",
+            dependencies: ["NIO", "NIOConcurrencyHelpers", "NIOSSL"]
+        ),
         .testTarget(
             name: "ElasticLogTests",
-            dependencies: ["ElasticLog"]),
+            dependencies: ["ElasticLog"]
+        ),
     ]
 )
